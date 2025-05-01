e474e86c75fa064b3772ada1caba83392ed8d54e
Sudhansu Bhushan Mishra <13695177+the-lazy-learner@users.noreply.github.com>
2025-05-01 13:48:06 +0530

    Add landing page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">Travel Log</NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a href="" class="btn btn-accent">
+        Sign In with GitHub
+        <Icon name="tabler:brand-github" size="24" />
+      </a>
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import NavBar from "~/components/app/nav-bar.vue";
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <NavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
