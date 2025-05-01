b822d8213d4fa47923746e6f68c75bb8fccfbc34
Sudhansu Bhushan Mishra <13695177+the-lazy-learner@users.noreply.github.com>
2025-05-01 14:41:21 +0530

    ADD theming

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -1,9 +1,14 @@
+<script setup lang="ts">
+import ThemeToggle from "./theme-toggle.vue";
+</script>
+
 <template>
   <div class="navbar bg-primary text-primary-content">
     <div class="navbar-start">
       <NuxtLink to="/" class="btn btn-ghost text-xl">Travel Log</NuxtLink>
     </div>
     <div class="navbar-end">
+      <ThemeToggle />
       <a href="" class="btn btn-accent">
         Sign In with GitHub
         <Icon name="tabler:brand-github" size="24" />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  colorMode: {
+    dataValue: "theme",
   },
 });
